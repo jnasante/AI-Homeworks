37a7d0a45ee177ae377ce042cb3c35f58840311b
Aric Kampschaefer <a.kampschaefer@ou.edu>
2016-03-21 00:34:15 -0500

    graphing and learning

--- hw4/hw4.py
+++ hw4/hw4.py
@@ -15,34 +15,37 @@ TMIN = data.TMIN
 TAVG = data.TAVG
 TMAX = data.TMAX
 
-data["DATE"] = data["MONTH"].map(str) + "-" + data["DAY"].map(str)
-actual["DATE"] = actual["MONTH"].map(str) + "-" + actual["DAY"].map(str)
+data["DATE"] = data["MONTH"].map(str) + "/" + data["DAY"].map(str)
+actual["DATE"] = actual["MONTH"].map(str) + "/" + actual["DAY"].map(str)
 
-# print(data["DATE"][0])
+data["DAYNUM"] = range(len(data["DATE"].map(str)))
+actual["DAYNUM"] = range(len(actual["DATE"].map(str)))
 
 # Generate beginning w array
 w = []
 for i in range(0, 4):
 	w.append(random.uniform(-1, 1))
 
+# Hold error over learning iterations
+err_over_time = []
+
 # Learn new w's
 def learn_w(alpha):
-	for _ in range(0, 10):
+	for _ in range(0, 100):
 		for row in range(0, data.shape[0]):
-			x = [data["DATE"][row], data["TMIN"][row], data["TAVG"][row]]
+			x = [1, float(data["DAYNUM"][row]), float(data["TMIN"][row]), float(data["TAVG"][row])]
 			actual_y = actual["TMAX"][row]
 			err = compute_error(w, x, actual_y)
+			if row == 0:
+				err_over_time.append( abs(err) )
 			for i in range(0, len(w)):
-				w[i] = lms(w[i], err, alpha, xij)
+				w[i] = lms(w[i], err, alpha, x[i])
 
 def lms(wi, error, alpha, xij):
-	return wi - alpha*error * xij
+	return wi - alpha * error * xij
 
 def predict(w, x):
-	return w[0] + w[1]*x[0] + w[2]*x[1] + w[3]*x[2]
-
-def predict_feature(w, xi):
-	return 
+	return w[0]*x[0] + w[1]*x[1] + w[2]*x[2] + w[3]*x[3]
 
 def compute_error(w, x, actual_y):
 	return predict(w, x) - actual_y
@@ -50,14 +53,40 @@ def compute_error(w, x, actual_y):
 # Define alpha
 alpha = 0.00001
 
-# f = predict(w, x)
+# Learn
+learn_w(alpha)
+
+# def test_predict(w, x):
+# 	for i in range(0, 4):
+# 		print("w[" + str(i) + "]: " + str(w[i]))
+# 		print("x[" + str(i) + "]: " + str(x[i]))
+# 	return w[0] + w[1]*x[1] + w[2]*x[2] + w[3]*x[3]
 
 
+# Create predictions for every day
 
-# fig, axs = plot.subplots(1, 3, sharey=True)
-# data.plot(kind='line', x='DATE', y='TMAX', ax=axs[0])
+f = []
+for day in range(0, data.shape[0]):
+	x = [1, float(data["DAYNUM"][day]), float(data["TMIN"][day]), float(data["TAVG"][day])]
+	f.append(predict(w, x))
+
+graph_x = actual["DATE"]
+y1 = actual["TMAX"]
+
+fig, axes = plot.subplots(nrows=2, ncols=1, sharey=False, sharex=False)
+axes[0].plot(y1)
+axes[0].plot(f)
+plot.sca(axes[0])
+plot.xticks(range(len(graph_x)), graph_x)
+
+axes[1].plot(range(len(err_over_time)), err_over_time)
+
+plot.show()
+
+
+# fig, axs = plot.subplots(1, 1, sharey=True)
+# actual.plot(kind='line', x='DATE', y='TMAX', ax=axs[0])
 # data.plot(kind='scatter', x='TMIN', y='TMAX', ax=axs[1])
 # data.plot(kind='scatter', x='TAVG', y='TMAX', ax=axs[2])
 # plot.show()
-
 
