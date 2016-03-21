907debdf804da20395cedf4fcb0ad98b4cf0a1bd
Aric Kampschaefer <a.kampschaefer@ou.edu>
2016-03-21 01:42:12 -0500

    fixed learning by using appropriate data for training and prediction, changed error calculation

--- hw4/hw4.py
+++ hw4/hw4.py
@@ -5,16 +5,6 @@ import random
 data = pd.read_csv('2015.csv')
 actual = pd.read_csv('2016.csv')
 
-# print(data.head())
-# print(data.shape[0])
-
-YEAR = data.YEAR
-MONTH = data.MONTH
-DAY = data.DAY
-TMIN = data.TMIN
-TAVG = data.TAVG
-TMAX = data.TMAX
-
 data["DATE"] = data["MONTH"].map(str) + "/" + data["DAY"].map(str)
 actual["DATE"] = actual["MONTH"].map(str) + "/" + actual["DAY"].map(str)
 
@@ -24,51 +14,46 @@ actual["DAYNUM"] = range(len(actual["DATE"].map(str)))
 # Generate beginning w array
 w = []
 for i in range(0, 4):
-	w.append(random.uniform(-1, 1))
+  w.append(random.uniform(-1, 1))
 
 # Hold error over learning iterations
 err_over_time = []
 
 # Learn new w's
 def learn_w(alpha):
-	for _ in range(0, 100):
-		for row in range(0, data.shape[0]):
-			x = [1, float(data["DAYNUM"][row]), float(data["TMIN"][row]), float(data["TAVG"][row])]
-			actual_y = actual["TMAX"][row]
-			err = compute_error(w, x, actual_y)
-			if row == 0:
-				err_over_time.append( abs(err) )
-			for i in range(0, len(w)):
-				w[i] = lms(w[i], err, alpha, x[i])
+  for _ in range(0, 100):
+    sum = 0.0
+    for row in range(0, data.shape[0]):
+      x = [1, data["DAYNUM"][row], data["TMIN"][row], data["TAVG"][row]]
+      y = data["TMAX"][row]
+      err = compute_error(w, x, y)
+      sum = sum + abs(err)
+        
+      for i in range(0, len(w)):
+        w[i] = lms(w[i], err, alpha, x[i])
+
+    err_over_time.append( sum/data.shape[0] )
 
 def lms(wi, error, alpha, xij):
-	return wi - alpha * error * xij
+  return wi - alpha * error * xij
 
 def predict(w, x):
-	return w[0]*x[0] + w[1]*x[1] + w[2]*x[2] + w[3]*x[3]
+  return w[0]*x[0] + w[1]*x[1] + w[2]*x[2] + w[3]*x[3]
 
-def compute_error(w, x, actual_y):
-	return predict(w, x) - actual_y
+def compute_error(w, x, y):
+  return predict(w, x) - y
 
 # Define alpha
 alpha = 0.00001
 
-# Learn
+# Learn w's
 learn_w(alpha)
 
-# def test_predict(w, x):
-# 	for i in range(0, 4):
-# 		print("w[" + str(i) + "]: " + str(w[i]))
-# 		print("x[" + str(i) + "]: " + str(x[i]))
-# 	return w[0] + w[1]*x[1] + w[2]*x[2] + w[3]*x[3]
-
-
 # Create predictions for every day
-
 f = []
 for day in range(0, data.shape[0]):
-	x = [1, float(data["DAYNUM"][day]), float(data["TMIN"][day]), float(data["TAVG"][day])]
-	f.append(predict(w, x))
+  x = [1, actual["DAYNUM"][day], actual["TMIN"][day], actual["TAVG"][day]]
+  f.append(predict(w, x))
 
 graph_x = actual["DATE"]
 y1 = actual["TMAX"]
@@ -89,4 +74,3 @@ plot.show()
 # data.plot(kind='scatter', x='TMIN', y='TMAX', ax=axs[1])
 # data.plot(kind='scatter', x='TAVG', y='TMAX', ax=axs[2])
 # plot.show()
-
