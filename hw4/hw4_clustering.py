62b902b992ee778afb3cebd79656eb7b20911182
jnasante <josephnasante@yahoo.com>
2016-03-21 21:28:12 -0500

    Fixed errors in code and cleaned up. Also finished update_centroids method.

--- hw4/hw4_clustering.py
+++ hw4/hw4_clustering.py
@@ -19,34 +19,39 @@ actual["DAYNUM"] = range(len(actual["DATE"].map(str)))
 data["LABEL"] = range(data.shape[0])
 
 # 
-data["EUCL"] = []
+data["EUCL"] = range(data.shape[0])
+
+# Calculate euclidean distance
+def euclidean_distance(daynum, tmin, tavg):
+  return math.sqrt( daynum**2 + tmin**2 + tavg**2 )
+
+# 
 for obs in range(0, data.shape[0]): 
-  data["EUCL"].append(euclidean_distance(data["DAYNUM"][obs], data["TMIN"][obs], data["TAVG"][obs]))
-
-# Test k values from 2 to 7
-for k in range(2, 8):
-	cluster(k)
-
-# Cluster with k centroids
-def cluster(k):
-  centroids = pick_centroids(k)
-  for _ in range(0, 10):
-    update_centroids(centroids, k)
-  return centroids
+  data["EUCL"][obs] = euclidean_distance(data["DAYNUM"][obs], data["TMIN"][obs], data["TAVG"][obs])
 
 # Create and return an array of k cluster centroids
 def pick_centroids(k):
   # Forgy Method - randomly select k observations to use as the initial centroids
-  centroids = np.array([])
+  centroids = [] # np.array([])
   rand_used = []
   for _ in range(0, k):
     obs = random.uniform(0, data.shape[0])
     while obs in rand_used:
       obs = random.uniform(0, data.shape[0])
+    
     rand_used.append(obs)
-    centroids.append( data["EUCL"][obs] )
+    centroids.append( data["EUCL"][int(obs)] )
 
   return centroids
+
+# 
+def closest_centroid(centroids, value):
+  closest = centroids[0]
+  for centroid in centroids:
+    if abs(centroid - value) < abs(closest - value):
+      closest = centroid
+
+  return closest
 
 # Assign the observations to the appropriate centroids
 def assign_data_to_centroids(centroids, k):
@@ -58,23 +63,31 @@ def assign_data_to_centroids(centroids, k):
 def update_centroids(centroids, k):
   sums = {}
   totals = {}
+  averages = []
+
+  for centroid in centroids:
+  	sums[str(centroid)] = 0
+  	totals[str(centroid)] = 0
+
   for obs in range(0, data.shape[0]):
     centroid = data["LABEL"][obs]
-    sums[centroid] = sums[centroid] + data["EUCL"][obs]
-    totals[centroid] = totals[centroid] + 1
-     
+    sums[str(centroid)] = sums[str(centroid)] + data["EUCL"][obs]
+    totals[str(centroid)] = totals[str(centroid)] + 1
 
-
-
-# Calculate euclidean distance
-def euclidean_distance(daynum, tmin, tavg):
-  return math.sqrt( daynum**2 + tmin**2 + tavg**2 )
-
-def closest_centroid(centroids, value):
-  closest = centroids[0]
   for centroid in centroids:
-    if abs(centroid - value) < abs(closest - value):
-      closest = centroid
+  	temp = sums[str(centroid)] / totals[str(centroid)]
+  	centroid = temp
 
-  return closest
+  return centroids
 
+# Cluster with k centroids
+def cluster(k):
+  centroids = pick_centroids(k)
+  assign_data_to_centroids(centroids, k)
+  for _ in range(0, 10):
+    update_centroids(centroids, k)
+  return centroids
+
+# Test k values from 2 to 7
+for k in range(2, 8):
+	cluster(k)
